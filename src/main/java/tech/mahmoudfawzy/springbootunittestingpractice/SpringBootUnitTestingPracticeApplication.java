98913f0e6008c4583661858f952978c654c7ec6e
Mahmoud Fawzy <mahmoudfawzykhalil98@gmail.com>
2022-07-27 18:04:29 +0200

    Initial commit

---
+++ src/main/java/tech/mahmoudfawzy/springbootunittestingpractice/SpringBootUnitTestingPracticeApplication.java
@@ -0,0 +1,11 @@
+package tech.mahmoudfawzy.springbootunittestingpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUnitTestingPracticeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootUnitTestingPracticeApplication.class, args);
+    }
+}
